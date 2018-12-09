3a0e4119452018ca8960a8895ace13029e5b4e93
milken <milken93@gmail.com>
2018-12-09 18:41:45 +0100

    rename MainActivity to SearchActivity, move it to search package

--- app/src/main/java/com/example/milken/githubsearchapp/search/SearchActivity.kt
+++ app/src/main/java/com/example/milken/githubsearchapp/search/SearchActivity.kt
@@ -1,9 +1,10 @@
-package com.example.milken.githubsearchapp
+package com.example.milken.githubsearchapp.search
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.milken.githubsearchapp.R
 
-class MainActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
