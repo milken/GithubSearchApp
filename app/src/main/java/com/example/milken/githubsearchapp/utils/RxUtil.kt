1e9b9c1a07c3d528a2b8e0b41c325df0f85c4a55
milken <milken93@gmail.com>
2018-12-11 21:19:07 +0100

    create RxUtil class with searchObservable transformation (previously from presenter) that applies debounce, distinct, filter and test it

---
+++ app/src/main/java/com/example/milken/githubsearchapp/utils/RxUtil.kt
@@ -0,0 +1,21 @@
+package com.example.milken.githubsearchapp.utils
+
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+class RxUtil(private val scheduler: Scheduler) {
+
+     fun searchObservableFrom(textChangeObservable: Observable<CharSequence>): Observable<String> {
+        return textChangeObservable
+            .debounce(DEBOUNCE_TIME, TimeUnit.MILLISECONDS, scheduler)
+            .distinct()
+            .filter { text -> !text.isBlank() }
+            .map { it.toString() }
+    }
+
+    companion object {
+        const val DEBOUNCE_TIME = 250L
+    }
+}
---
+++ app/src/test/java/com/example/milken/githubsearchapp/utils/RxUtilTest.kt
@@ -0,0 +1,76 @@
+package com.example.milken.githubsearchapp.utils
+
+import io.reactivex.schedulers.TestScheduler
+import io.reactivex.subjects.PublishSubject
+import org.junit.Before
+import org.junit.Test
+import java.util.concurrent.TimeUnit
+
+class RxUtilTest {
+
+    private val testScheduler = TestScheduler()
+
+    private val rxUtil = RxUtil(testScheduler)
+
+    private val subject = PublishSubject.create<CharSequence>()
+    private val testObserver = rxUtil.searchObservableFrom(subject).test()
+
+    private val debounceTime = RxUtil.DEBOUNCE_TIME
+    private val lessThanDebounceTime = (0.5 * RxUtil.DEBOUNCE_TIME).toLong()
+
+    private val emptyValue = ""
+    private val firstValue = "a"
+    private val secondValue = "abc"
+
+    @Test
+    fun searchObservableFrom_singleValue(){
+        subject.onNext(firstValue)
+        testScheduler.advanceTimeBy(debounceTime, TimeUnit.MILLISECONDS)
+
+        testObserver.assertValues(firstValue)
+    }
+
+    @Test
+    fun searchObservableFrom_twoValuesEmitsLast() {
+        subject.onNext(firstValue)
+        testScheduler.advanceTimeBy(lessThanDebounceTime, TimeUnit.MILLISECONDS)
+
+        subject.onNext(secondValue)
+        testScheduler.advanceTimeBy(debounceTime, TimeUnit.MILLISECONDS)
+
+        testObserver.assertValues(secondValue)
+    }
+
+    @Test
+    fun searchObservableFrom_twoValuesEmitsBoth() {
+        subject.onNext(firstValue)
+        testScheduler.advanceTimeBy(debounceTime, TimeUnit.MILLISECONDS)
+
+        subject.onNext(secondValue)
+        testScheduler.advanceTimeBy(debounceTime, TimeUnit.MILLISECONDS)
+
+        testObserver.assertValues(firstValue, secondValue)
+    }
+
+    @Test
+    fun searchObservableFrom_emptyValueEmitsNothing() {
+        subject.onNext(emptyValue)
+        testScheduler.advanceTimeBy(debounceTime, TimeUnit.MILLISECONDS)
+
+        testObserver.assertValues()
+    }
+
+    @Test
+    fun searchObservableFrom_assertRepeatEmitsOnce() {
+        subject.onNext(secondValue)
+        testScheduler.advanceTimeBy(debounceTime, TimeUnit.MILLISECONDS)
+
+        subject.onNext(firstValue)
+        testScheduler.advanceTimeBy(lessThanDebounceTime, TimeUnit.MILLISECONDS)
+
+        subject.onNext(secondValue)
+        testScheduler.advanceTimeBy(debounceTime, TimeUnit.MILLISECONDS)
+
+        testObserver.assertValues(secondValue)
+    }
+}
