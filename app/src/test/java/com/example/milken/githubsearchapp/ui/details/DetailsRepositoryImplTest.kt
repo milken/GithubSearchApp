8abe1373c2eb9e7a725c17c40defbafd7c1945fd
milken <milken93@gmail.com>
2018-12-12 22:17:30 +0100

    create detailsRepository tests - it's pretty much the same as searchRepository tests - TODO: extract base later

---
+++ app/src/test/java/com/example/milken/githubsearchapp/ui/details/DetailsRepositoryImplTest.kt
@@ -0,0 +1,89 @@
+package com.example.milken.githubsearchapp.ui.details
+
+import com.example.milken.githubsearchapp.data.apis.GithubUserDetailsApi
+import com.example.milken.githubsearchapp.data.common.RequestCallback
+import com.example.milken.githubsearchapp.data.models.User
+import com.example.milken.githubsearchapp.data.models.UsersResponse
+import com.example.milken.githubsearchapp.utils.ErrorParser
+import com.example.milken.githubsearchapp.utils.SchedulerProviderFake
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import okhttp3.MediaType
+import okhttp3.ResponseBody
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+import retrofit2.HttpException
+import retrofit2.Response
+import java.lang.Exception
+
+class DetailsRepositoryImplTest {
+
+    private val requestCallback = mockk<RequestCallback<User>>(relaxed = true)
+    private val userDetailsApi = mockk<GithubUserDetailsApi>(relaxed = true)
+    private val schedulerProvider = SchedulerProviderFake()
+    private val errorParser = ErrorParser()
+
+    private val currentDisposableMock = mockk<Disposable>(relaxed = true)
+
+    private val detailsRepository = DetailsRepositoryImpl(userDetailsApi, schedulerProvider, errorParser)
+
+    private val user = User(id = 1, login = "Adam", avatarUrl = "avatar_url", detailsUrl = "url", followersCount = 10)
+    private val query = "login"
+
+    @Before
+    fun setUp() {
+        detailsRepository.setRequestCallback(requestCallback)
+    }
+
+    @Test
+    fun fetchUserDetails_assertRequestSuccess_sendUserData() {
+        every { userDetailsApi.getUserDetails(any()) } returns Observable.just(user)
+
+        detailsRepository.fetchUserDetails(query)
+
+        verify {
+            requestCallback.requestSuccess(user)
+        }
+    }
+
+
+    @Test
+    fun fetchDataWith_assertRequestFailsWithException_callsRequestErrorWithCorrectMessage() {
+        val message = "error"
+        val exception = Exception(message)
+        every { userDetailsApi.getUserDetails(any()) } returns Observable.error(exception)
+
+        detailsRepository.fetchUserDetails(query)
+
+        verify {
+            requestCallback.requestError(message)
+        }
+    }
+
+    @Test
+    fun fetchDataWith_assertRequestFailsWithHttp403Exception_callsRequestErrorWithCorrectMessage() {
+        val exception = HttpException(Response.error<Any>(403, ResponseBody.create(MediaType.parse("text/plain"), "Forbidden")))
+        every { userDetailsApi.getUserDetails(any()) } returns Observable.error(exception)
+
+        detailsRepository.fetchUserDetails(query)
+
+        verify {
+            requestCallback.requestError(ErrorParser.error403Message)
+        }
+    }
+
+    @Test
+    fun viewDestroyed() {
+        detailsRepository.currentRequestDisposable = currentDisposableMock
+        detailsRepository.viewDestroyed()
+
+        verify {
+            currentDisposableMock.dispose()
+        }
+    }
+}
